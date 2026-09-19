773c6ddd20de459a0250c14c9c9f7a6fd3257282
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 14:51:05 -0700

    feat: initialize weekly menu countdown

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ import { initCalculator } from "./features/calculator.js";
 import { initCarousel } from "./features/carousel.js";
 import { initForms } from "./features/forms.js";
 import { renderMenu } from "./features/menu.js";
+import { initTimer } from "./features/timer.js";
 import { initModal } from "./ui/modal.js";
 import { initTabs } from "./ui/tabs.js";
 
 initTabs();
+initTimer();
 const modal = initModal();
 
 renderMenu();
